1cb31d0e90d967e838494e75a367f09360997ad5
Mauricio Mercado <me@maumercado.com>
2022-12-11 17:48:17 -0800

    Try again

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-// run `node index.js` in the terminal
+import dotenv from 'dotenv';
+import { Client } from '@notionhq/client';
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+dotenv.config();
+
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
+
+const { results: databases } = await notion.search({
+  filter: {
+    value: 'database',
+    property: 'object',
+  },
+});
+
+const databaseIds = databases.map((database) => database.id)
+
+console.log(databaseIds)
